57a095fb23b7b7ce6665d92c2e8f693ef2774f8a
JBDiel <79658699+JBDiel@users.noreply.github.com>
2022-08-08 01:23:06 +0100

    Add files via upload

---
+++ 2mfossept/2mfossept.js
@@ -0,0 +1,7 @@
+const  nous=document.querySelector(".click-event");
+const btn=document.querySelector("click-event");
+const reponse = document.getElementById("p")
+btn.addEventListener('clikc',()=>{
+   reponse.style.visibility="visible";
+})
+
